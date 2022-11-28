08ec0d81eeb87f9ecaafa5ec084662da09b53ab5
Rohan <rohanmathur@berkeley.edu>
2022-11-28 10:46:02 -0800

    segmenting forearms

--- process_segmentation.py
+++ process_segmentation.py
@@ -40,7 +40,6 @@ def get_left_hand(im):
 	all_hands = np.zeros_like(all_lefts)
 	all_hands[all_lefts == 1] = 1
 	all_hands[all_rights == 1] = 1
-	utils.show_image(all_hands)
 	left_hand = np.zeros_like(all_hands)
 	all_hand_edges = edge_detector(all_hands)
 	left_indices, right_indices = segment_clusters_by_x(all_hand_edges)
@@ -59,10 +58,38 @@ def get_right_hand(im):
 	right_hand[right_inds[:,0], right_inds[:,1]] = 1
 	return right_hand
 
+def get_left_forearm(im):
+	left_fronts = get_body_part_mask(im, 6)
+	left_backs = get_body_part_mask(im, 7)
+	right_fronts = get_body_part_mask(im, 8)
+	right_backs = get_body_part_mask(im, 9)
+	all_lower_arms = np.zeros_like(left_fronts)
+	all_lower_arms[(left_fronts==1)|(left_backs==1)|(right_fronts==1)|(right_backs==1)] = 1
+	left_forearm = np.zeros_like(all_lower_arms)
+	all_forearm_edges = edge_detector(all_lower_arms)
+	left_inds, right_inds = segment_clusters_by_x(all_forearm_edges)
+	left_forearm[left_inds[:,0], left_inds[:,1]] = 1
+	return left_forearm
+
+def get_right_forearm(im):
+	left_fronts = get_body_part_mask(im, 6)
+	left_backs = get_body_part_mask(im, 7)
+	right_fronts = get_body_part_mask(im, 8)
+	right_backs = get_body_part_mask(im, 9)
+	all_lower_arms = np.zeros_like(left_fronts)
+	all_lower_arms[(left_fronts==1)|(left_backs==1)|(right_fronts==1)|(right_backs==1)] = 1
+	right_forearm = np.zeros_like(all_lower_arms)
+	all_forearm_edges = edge_detector(all_lower_arms)
+	left_inds, right_inds = segment_clusters_by_x(all_forearm_edges)
+	right_forearm[right_inds[:,0], right_inds[:,1]] = 1
+	return right_forearm
+
 if __name__ == '__main__':
 	im = skio.imread("./joe_segmented.png", as_gray=True)
 	utils.show_image(im)
 	#left_hand = get_left_hand(im)
 	#utils.show_image(left_hand)
-	right_hand = get_right_hand(im)
-	utils.show_image(right_hand)
+	#right_hand = get_right_hand(im)
+	#utils.show_image(right_hand)
+	right_forearm = get_right_forearm(im)
+	utils.show_image(right_forearm)
