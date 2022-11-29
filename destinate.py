d9430b0242cadae5edca5f9024662395cd03fb39
Joy Liu <liujoy57@gmail.com>
2022-11-28 23:40:54 -0800

    calculate left hand deestination

--- destinate.py
+++ destinate.py
@@ -4,45 +4,68 @@ import skimage.io as skio
 import triangulate as tri
 import numpy as np
 from scipy import signal
+from scipy.ndimage import binary_fill_holes 
+import skimage
 
-def get_end_points(pts):
-	return pts[0], pts[-1] 
+from scipy.interpolate import RectBivariateSpline
 
-r = 25 
+def pts_to_im(im, pts):
+	new_im = np.zeros_like(im)
+	new_im[pts[:,0], pts[:,1]] = 1
+	return new_im
+
+def im_to_pts(im):
+	return np.argwhere(im)
+
+def get_end_points(border, shared_border):
+	nonshared_border = border - shared_border
+	end_points = np.argwhere(bpt.find_border(shared_border, nonshared_border))
+	return [end_points[0], end_points[-1]]
+
+r_1 = 100
+r_2 = 10 
 
 joe = skio.imread("joe2.jpg")
 joe_segs = skio.imread("joe_seg_crop2.png", as_gray=True)
 left_hand = bpt.construct_left_hand(joe_segs)
-hand_border = left_hand.general_points
-print(hand_border.shape)
-
-border_sampled = hand_border[::hand_border.shape[0]//10]
+border = left_hand.general_points
 shared_border = left_hand.get_border("left_forearm")
-end_points = get_end_points(joe_segs, shared_border)
 
-dest_geometry = []
-grad_x, grad_y = np.gradient(joe_segs)
+border_im = pts_to_im(joe_segs, border)
+shared_border_im = pts_to_im(joe_segs, shared_border)
+nonshared_border = im_to_pts(border_im - shared_border_im)
 
-for point in border_sampled: 
-	dx = grad_x[point[0]]
-	dy = grad_y[point[1]]
+border_sampled = nonshared_border[::nonshared_border.shape[0]//15]
+end_points = get_end_points(border_im, shared_border_im)
+start_geometry = np.vstack((border_sampled, np.array(end_points)))
+
+target_geometry = []
+filled_border = binary_fill_holes(border_im).astype(int)
+grad_x, grad_y = np.gradient(skimage.filters.gaussian(filled_border, sigma=(3, 3), truncate=2))
+
+for point in border_sampled:
+	dx = grad_x[point[0], point[1]]
+	dy = grad_y[point[0], point[1]]
 	mag = (dx**2 + dy**2)**0.5
 	dx /= mag 
 	dy /= mag 
-	if point in shared_border: 
+	target_geometry.append([point[0] - dx * r_1, point[1] - dy * r_1])
 
-	else: 
-		dest_geometry.append([point[0] + dx * r, point[1] + dy * r])
-dest_geometry = np.array(dest_geometry)
+for point in end_points: 
+	target_geometry.append([point[0] - dx * r_2, point[1] - dy * r_2])
+target_geometry = np.array(target_geometry)
 
-plt.imshow(joe)
-plt.scatter(border_sampled[:,0], border_sampled[:,1], "r")
-plt.scatter(dest_geometry[:,0], dest_geometry[:,1], "g")
+plt.imshow(joe_segs)
+plt.scatter(start_geometry[:,1], start_geometry[:,0], s=5, c="r")
+plt.scatter(target_geometry[:,1], target_geometry[:,0], s=5, c="b")
 plt.show()
+
+triangulation = tri.triangulate_scipy(start_geometry)
+print(triangulation)
 
 # TESTING FOR ROHAN
 # im = np.zeros_like(joe_segs)
-# im[hand_border[:,0], hand_border[:,1]] = 1
+# im[border[:,0], border[:,1]] = 1
 # plt.imshow(im)
 # plt.show()
 
